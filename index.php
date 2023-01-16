fb4127446fa75541f9845127022cc4bf9561f777
ry3soul <121807404+ry3soul@users.noreply.github.com>
2023-01-16 11:20:26 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
+//
+// check if credentials passed are in db
+//
+function checkCreds($conn, $email, $password) {
+	$sql = "SELECT id FROM users";
+		$sql = $sql . " where email='" . $email . "' AND password='" . $password . "';";
+	$result = mysqli_query($conn, $sql);
+
+	// if the query returns only one result, email and password are OK
+	if (mysqli_num_rows($result) == 1) {
+		$status = True;
+	} else {
+		$status = False;
+	}
+
+	// Close the connection 
+	mysqli_close($conn);
+	return $status;	
+}
+
+//
+// Process bad login
+//
+function processBadLogin() {
+	$_SESSION["status"] = "NotLoggedIn";
+	$_SESSION['login_error_msg'] = "Sorry, that user name or password is incorrect. Please try again.";
+	header("Location: index.php");
+	exit();		
+}
 
